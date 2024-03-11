4ef701f12510551ae3e130a215aae1fd5056d2a0
anzaeee <anzaycx@gmail.com>
2024-03-11 14:07:02 +0500

    finalizing widget

---
+++ src/lib/components/WhatsappWidget.svelte
@@ -0,0 +1,40 @@
+<script lang='ts'>
+  import WhatsappLogo from "$lib/images/WhatsappLogo.svelte";
+
+  function openWhatsAppChat() {
+    const phoneNumber = 'your-phone-number'; // Replace with your actual phone number
+    const url = `https://wa.me/${phoneNumber}`;
+    window.open(url, '_blank', 'width=500,height=600'); // Adjust width and height as needed
+  }
+</script>
+
+<div class="whatsapp-container" on:click={openWhatsAppChat} role="button" tabindex="0">
+  <WhatsappLogo/>
+  <div class="whatsapp-text">
+    Chat With Us
+  </div>
+</div>
+
+<style>
+  .whatsapp-container {
+    position: relative;
+    display: inline-block;
+    cursor: pointer;
+  }
+
+  .whatsapp-text {
+    display: none;
+    position: absolute;
+    background-color: #ffffff;
+    padding: 5px;
+    border-radius: 5px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+    bottom: -30px;
+    right: -30px;
+    z-index: 1;
+  }
+
+  .whatsapp-container:hover .whatsapp-text {
+    display: inline-block;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,16 @@
   import "../app.css";
   import Header from "$lib/components/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
+  import WhatsappWidget from "$lib/components/WhatsappWidget.svelte";
 </script>
 
-<div class="space-y-10">
-  <Header />
-  <slot />
-  <Footer />
+<Header />
+<slot />
+
+<div class="relative">
+  <div class="absolute bottom-0 right-5">
+    <WhatsappWidget/>
+  </div>
 </div>
+
+<Footer />
