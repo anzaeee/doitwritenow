e3646f957402bec3cb231a56950b66b5e284b510
Shanzay <shanzayyasad@gmail.com>
2024-03-12 17:09:07 +0500

    changes to header

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -2,9 +2,7 @@
   import HeaderItem from "./HeaderItem.svelte";
 </script>
 
-<div
-  class="flex flex-row w-screen h-auto justify-between my-5 justify-center"
->
+<div class="flex flex-row w-screen h-auto justify-between my-5 justify-center">
   <div class="flex flex-row gap-4 p-2 ml-10">
     <HeaderItem link="/">Home</HeaderItem>
     <HeaderItem link="/about">About</HeaderItem>
@@ -13,6 +11,8 @@
   </div>
   <div class="flex flex-row gap-4 p-2 mr-10">
     <HeaderItem link="/contact-us">Contact Us</HeaderItem>
-    <HeaderItem link="https://wa.me/923369555887">Chat With Us</HeaderItem>
+    <HeaderItem link="https://wa.me/923369555887" target="_blank"
+      >Chat With Us</HeaderItem
+    >
   </div>
 </div>
