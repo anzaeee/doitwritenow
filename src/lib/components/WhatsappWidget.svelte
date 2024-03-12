aca2c5b16a2087151201ded96e5014f669488006
anzaeee <anzaycx@gmail.com>
2024-03-12 11:14:39 +0500

    widget updates

--- src/lib/components/WhatsappWidget.svelte
+++ src/lib/components/WhatsappWidget.svelte
@@ -15,7 +15,6 @@
   tabindex="0"
 >
   <WhatsappLogo />
-  <div class="whatsapp-text">Chat With Us</div>
 </div>
 
 <style>
@@ -23,21 +22,5 @@
     position: relative;
     display: inline-block;
     cursor: pointer;
-  }
-
-  .whatsapp-text {
-    display: none;
-    position: absolute;
-    background-color: #ffffff;
-    padding: 5px;
-    border-radius: 5px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
-    bottom: -30px;
-    right: -30px;
-    z-index: 1;
-  }
-
-  .whatsapp-container:hover .whatsapp-text {
-    display: inline-block;
   }
 </style>
