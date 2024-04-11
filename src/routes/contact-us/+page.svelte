d2a009ea99bc67850abe217a2c177bcf9ea2472d
Shanzay <shanzayyasad@gmail.com>
2024-04-11 13:07:36 +0500

    fixing the submit form

--- src/routes/contact-us/+page.svelte
+++ src/routes/contact-us/+page.svelte
@@ -1,74 +1,96 @@
 <script>
   let status = "";
-  const handleSubmit = async event => {
-    event.preventDefault(); // Prevent form submission
-    
-    status = 'Submitting...';
-    const formData = new FormData(event.currentTarget);
+  const handleSubmit = async (data) => {
+    status = "Submitting...";
+    const formData = new FormData(data.currentTarget);
     const object = Object.fromEntries(formData);
     const json = JSON.stringify(object);
 
-    try {
-      const response = await fetch("https://api.web3forms.com/submit", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-          "Accept": "application/json",
-        },
-        body: json
-      });
-      
-      const result = await response.json();
-      if (result.success) {
-        console.log(result);
-        status = result.message || "Success";
-      } else {
-        status = "Submission failed";
-      }
-    } catch (error) {
-      console.error("Error:", error);
-      status = "Error submitting form";
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: json,
+    });
+    const result = await response.json();
+    if (result.success) {
+      console.log(result);
+      status = result.message || "Success";
     }
-  }
+  };
 </script>
 
-<div class="space-y-8 mx-auto px-4 md:px-8 lg:px-16 xl:px-20 min-h-screen justify-center align-center">
-  <div class="flex flex-col justify-center items-center space-y-8 gap-10 border-t">
-    <div class="text-4xl md:text-6xl mt-8 text-primary font-bold space-y-8 fade-in text-center">
-      Contact Us
-    </div>
-    <div class="text-xl md:text-2xl space-y-4 font-medium text-neutral-750 fade-in">
-      Have a question? Reach out to us! We are eager to help you.
-    </div>
-    <form on:submit|preventDefault={handleSubmit} class="form-group px-2 w-full md:w-3/4 lg:w-1/2 xl:w-1/3 fade-in">
-      <div class="flex flex-col md:flex-row w-full">
-      <input type="hidden" name="access_key" value="808d2cef-012e-425a-aec5-65e9e067b1c1">
-        <input id="nameInput" name="name" class="rounded-input bg-slate-100 w-full mb-4 md:mb-0 md:mr-4" type="text" placeholder="Name" required />
-        <input id="emailInput" name="email" class="rounded-input bg-slate-100 w-full" type="email" placeholder="Email" required />
-      </div>
-      <div class="form-group mt-5">
-        <textarea id="messageInput" name="message" class="rounded-input bg-slate-100 w-full" placeholder="Message" rows="4" required></textarea>
-      </div>
-
-      <button id="submitButton" type="button" class="bg-slate-800 hover:bg-slate-900 text-white rounded-input cursor-pointer w-full" onclick="sendEmail()">
-        Send Message
-      </button>
-    </form>
+<div class="flex flex-col justify-center items-center gap-10 mt-10">
+  <div class="text-4xl md:text-6xl text-primary font-bold fade-in">
+    Contact Us
+  </div>
+  <div
+    class="text-xl md:text-2xl space-y-4 font-medium text-neutral-750 fade-in"
+  >
+    Send us a message and we'll get back to you as soon as possible.
   </div>
 </div>
 
-<style>
-  .rounded-input {
-    border-radius: 0.5rem;
-    padding: 0.5rem;
-    border: 1px solid #ccc;
-  }
+<form
+  class="max-w-md mx-auto p-6 bg-white rounded-md shadow-md h-full m-8 w-full"
+  on:submit|preventDefault={handleSubmit}
+>
+  <input
+    type="hidden"
+    name="access_key"
+    value="a44e48a9-fba9-48bc-b6ac-a51cd6a483f1"
+  />
 
-  .form-group {
-    margin-bottom: 1rem;
-  }
+  <div class="mb-4">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="name"
+      >Name:</label
+    >
+    <input
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="name"
+      type="text"
+      name="name"
+      placeholder="Enter your name"
+      required
+    />
+  </div>
 
-  *:focus {
-    outline: none;
-  }
-</style>
+  <div class="mb-4">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="email"
+      >Email:</label
+    >
+    <input
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="email"
+      type="email"
+      name="email"
+      placeholder="Enter your email"
+      required
+    />
+  </div>
+
+  <div class="mb-4">
+    <label class="block text-gray-700 text-sm font-bold mb-2" for="message"
+      >Message:</label
+    >
+    <textarea
+      class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+      id="message"
+      name="message"
+      placeholder="Enter your message"
+      required
+      rows="3"
+    ></textarea>
+  </div>
+
+  <div class="flex items-center justify-between">
+    <button
+      class="bg-gray-700 hover:bg-gray-900 w-full text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      type="submit">Submit</button
+    >
+  </div>
+</form>
+
+<div class="text-center mt-4">{status}</div>
